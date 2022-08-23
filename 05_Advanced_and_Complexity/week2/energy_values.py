fe0eecd1a2a79fa7bba08b472a6cd0b82afaee70
Alexandr <tori.bicks@gmail.com>
2022-08-23 21:22:56 +0300

    A5w2p1 added

--- 05_Advanced_and_Complexity/week2/energy_values.py
+++ 05_Advanced_and_Complexity/week2/energy_values.py
@@ -29,32 +29,15 @@ def ReadEquation():
         a.append(line[:size])
         b.append(line[size])
 
-    if size == 0:
-        exit(0)
-    elif size == 1:
-        print(b[0] / a[0])
-        exit(0)
-
     return Equation(a, b)
 
 
-def SelectPivotElement(a, used_rows, used_columns, piv_elem:Position, size):
-    while used_rows[piv_elem.row] or not a[piv_elem.row][piv_elem.column]:
+def SelectPivotElement(a, used_rows, piv_elem:Position, size):
+    while piv_elem.row < size and (used_rows[piv_elem.row] or abs(a[piv_elem.row][piv_elem.column]) < EPS):
         piv_elem.row += 1
-    # piv_elem = Position(0, 0)
-    # while used_columns[piv_elem.column]:
-    #     piv_elem.column += 1
-    # while used_rows[piv_elem.row]:
-    #     piv_elem.row += 1
-
-    # while not a[piv_elem.row][piv_elem.column]:
-    #     if piv_elem.column + 1 < size and used_columns[piv_elem.column]:
-    #         piv_elem.column += 1
-    #     elif piv_elem.row + 1 < size:
-    #         piv_elem.row += 1
-    #     else:
-    #         used_columns[piv_elem.column] = True
-    #         break
+    
+    if piv_elem.row == size:
+        return False
 
     return piv_elem
 
@@ -67,78 +50,55 @@ def SwapLines(a, b, used_rows, piv_elem:Position):
 
 
 def ProcessPivotElement(a, b, piv_elem:Position, size, used_rows):
-    # find divider and pivot row
-    row = a[piv_elem.row]
+    # find divider
     div = a[piv_elem.row][piv_elem.column]
     # make pivot element equals 1
     if div != 1:
-        a[piv_elem.row] = [num / div for num in row]
+        for i in range(piv_elem.column, size):
+            a[piv_elem.row][i] /= div
+        b[piv_elem.row] /= div
 
-        b[piv_elem.row] = b[piv_elem.row] / div
-        row = a[piv_elem.row]
-
-
-    # substruct pivot row from every other row to make 0 in pivot column
+     # substruct pivot row from every other row to make 0 in pivot column
     for i in range(size):
-        if i != piv_elem.row:
-            # define multiplier
-            mult = a[i][piv_elem.column]
-            if mult:
-                # substruct 'row' from every row
-                for j in range(piv_elem.column, size):
-                    a[i][j] -= row[j] * mult
-                b[i] -= b[piv_elem.row] * mult
+        mult = a[i][piv_elem.column]
+        if i == piv_elem.row or mult == 0:
+            continue
+        else:
+            # substruct 'row' from every row
+            for j in range(size):
+                a[i][j] -= a[piv_elem.row][j] * mult
+            b[i] -= b[piv_elem.row] * mult
                 
-    # piv_elem.proc.add(piv_elem.row)
-    # # increase pivot element
-    # piv_elem.row += 1
-    # piv_elem.column += 1
     used_rows[piv_elem.row] = True
 
     return piv_elem
 
 
-# def MarkPivotElementUsed(piv_elem:Position, used_rows, used_columns):
-#     used_rows[piv_elem.row] = True
-#     used_columns[piv_elem.column] = True
-
-
 def SolveEquation(equation:Equation):
+    """
+    Solves system of linear equations represented by square matrix 'a'
+    and vector 'b' using Gauss Jordan Elimination method
+    """
     a = equation.a
     b = equation.b
     size = len(a)
 
-    used_columns = [False] * size
     used_rows = [False] * size
-    # while piv_elem.row < size and piv_elem.column < size:
     for step in range(size):
         piv_elem = Position(step, 0)
-        # piv_elem.column = step
-        piv_elem = SelectPivotElement(a, used_rows, used_columns, piv_elem, size)
-        # if not a[piv_elem.row][piv_elem.column]:
-        #     continue
+        piv_elem = SelectPivotElement(a, used_rows, piv_elem, size)
+        if not piv_elem:
+            return None
         SwapLines(a, b, used_rows, piv_elem)
         piv_elem = ProcessPivotElement(a, b, piv_elem, size, used_rows)
-        # MarkPivotElementUsed(piv_elem, used_rows, used_columns)
 
     return b
 
-
-# def PrintColumn(column):
-    # size = len(column)
-    # print("{0:.6f}".format(column[row]), sep=' ', end='\n')
-    # for row in range(size):
-        # if used_columns[row] and used_rows[row]:
-        # else:
-        #     print(0, end=' ')
-        # # print("%.20lf" % column[row])
-    # print()
 
 if __name__ == "__main__":
     equation = ReadEquation()
     solution = SolveEquation(equation)
     print(" ".join(["{0:.6f}".format(ans) for ans in solution]))
-    # PrintColumn(solution)
     exit(0)
 
 
